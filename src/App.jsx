b4877f090734f48759b60700066c7a1c6a9b9692
SilviaFranzinelli <franzinellisilvia@gmail.com>
2025-02-04 18:48:14 +0100

    finito

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 
+
 import './App.css'
+import AllTheBook from './components/AllTheBook'
+import MyAlert from './components/MyAlert'
+import MyFooter from './components/MyFooter'
+import MyNav from './components/MyNav'
 
 function App() {
  
 
   return (
     <>
-      
+      <MyNav/>
+      <MyAlert/>
+      <AllTheBook/>
+      <MyFooter/>
     </>
   )
 }
